0c092f8485cc2917828cc40b82d414712c4af93c
Edwin Suarez <edwin.suarez@gsdplus.com>
2021-04-13 22:38:57 -0500

    CRUD con mongodb y node js proyecto netamente de backend, no contiene integración de front

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-//Requerimos mongo ya previamente instalado
-var MongoClient = require('mongodb');
-var url = "mongodb://localhost:27017/mydb";
+var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
+MongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
+
+    let dataBase = db.db('Books')
+
+    if (err) {
+        throw err;
+    }
+
+    function obtenerDatos() {
+        dataBase.collection('Categories').find()
+    }
+
+    console.log(
+        obtenerDatos()
+    );
+
+    dataBase.collection('Categories').insertOne({
+        name: "Religioso",
+        description: "Religioso"
+    }).then(function (resultado) {
+        console.log(resultado);
+    })
+
+    dataBase.collection('Categories').updateMany( 
+        {name: "Religioso"}, 
+        {$set: 
+            {name: 'Religion', description: 'Cristianismo'}
+    }).then( function(actualizado){
+        console.log(actualizado);
+    })
+
+    dataBase.collection('Categories').deleteMany({
+        name: "Religion"
+    }).then(function(eliminado){
+        console.log(eliminado);
+    })
+
+    db.close()
+
 });
